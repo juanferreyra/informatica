8b13559f7efad95368ac3f9994e1cbd1e95ffcda
Juan Ferreyra <juangabrielferreyra93@gmail.com>
2016-08-31 13:42:42 -0300

    algunos mensajees

--- presentacion/menu/index.php
+++ presentacion/menu/index.php
@@ -15,6 +15,40 @@ $usuario = $_SESSION['usuario'];
 $data = unserialize($usuario);
 
 /*fin de agregado usuario*/
+// Completamos el vector con frases
+$vector = array(
+1 => "Nunca me decepcionas.",
+2 => "Me encanta que me mimen y tú sabes mimarme todo el tiempo.",
+3 => "La sabiduría es la hija de la experiencia.",
+4 => " Jamás conocí a un hombre tan ambicioso y determinado como tú.",
+5 => "Anoche estabas bien sexy con esa camisa.",
+6 => "Si estuvieras aquí no tendría frío, ¿qué dices?",
+7 => "Quiero desabrocharte esa camisa que llevas puesta.",
+8 => "Mi cama está fría, ¿quieres hacer algo al respecto?",
+9 => "Quisiera que pudieras ver lo que llevo puesto ahora",
+10 => "Esta noche mis labios no dejaran ni un espacio de tu cuerpo en paz.",
+11 => "Cuando todo sube, lo único que baja es la ropa interior.",
+12 => "No puedo esperar a hacerte las cosas que estoy pensando.",
+13 => "Te quiero ahora, pero adentro mio",
+14 => "Que ganas de desabrochar algo, y recorrer con mis manos todo tu...",
+15 => "Mis amigos están celosos de mí por tener un relación tan perfecta contigo.",
+16 => "Vaya, te ves tan sexy. Todas las mujeres te están viendo.",
+17 => "Tú eres el tipo de hombre que toda mujer sueña tener.",
+18 => "Cuando estoy confundida sobre algo, me pregunto lo que habrías hecho en mi lugar.",
+19 => "Sólo la idea de estar contigo en la cama me produce un cosquilleo.",
+20 => "Te ves tan bien que haces que todas las mujeres de la sala me tengan envidia.",
+21 => "Tu olor me excita. ¿Puedo llevarme tu camisa para dormir con ella cuando te vayas de viaje?",
+22 => "Nunca me decepcionas.",
+23 => "Me encanta que me mimen y tú sabes mimarme todo el tiempo.",
+24 => "Eres tan inteligente ¿Cómo haces para siempre tener las respuestas a mis preguntas?",
+25 => "Eres un hombre muy interesante. Sabes mucho sobre todo.",
+26 => "Eres tan tierno y dulce que me haces sentir como a una gatita mimada.",
+);
+
+// Obtenemos un número aleatorio
+$numero = rand(1,26);
+
+// Imprimimos la frase
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -44,12 +78,13 @@ $data = unserialize($usuario);
 		<script type="text/javascript">
 			var icono = <?php echo "'".$data->getLogo()."'"; ?>;
 	        var iconoDetalle = <?php echo "'".$data->getLogoDetalle()."'"; ?>;
+	        var texto = <?php echo "'".$vector[$numero]."'"; ?>;
 
 			$(document).ready(function(){
 				//NOTIFICACION
 		        // create the notification
 		        var notification = new NotificationFx({
-		            message : '<div class="ns-thumb"><img src="../includes/plug-in/notificacion/img/'+icono+'"/></div><div class="ns-content"><p><a href="#">'+iconoDetalle+'&nbsp;</a>Hola Hermoso!</p></div>',
+		            message : '<div class="ns-thumb"><img src="../includes/plug-in/notificacion/img/'+icono+'"/></div><div class="ns-content"><p><a href="#">'+iconoDetalle+'&nbsp;</a>'+texto+'</p></div>',
 		            layout : 'other',
 		            ttl : 6000,
 		            effect : 'thumbslider',
